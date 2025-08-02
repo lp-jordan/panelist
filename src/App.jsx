73921f77b61b470cd9bafd1506a988f43994a154
lp-jordan <jordan@leaderpass.com>
2025-08-02 00:15:53 -0400

    Add custom Tiptap nodes and slash command

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import SlashCommand from './extensions/SlashCommand'
+import {
+  PageHeader,
+  PanelHeader,
+  Description,
+  Dialogue,
+  Sfx,
+  NoCopy,
+} from './extensions/customNodes'
 
 export default function App() {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      PageHeader,
+      PanelHeader,
+      Description,
+      Dialogue,
+      Sfx,
+      NoCopy,
+      SlashCommand,
+    ],
     content: '',
   })
 
