1c569515ae0c2c1e936d3291e8ed36affd2a0f03
lp-jordan <jordan@leaderpass.com>
2025-08-03 14:12:29 -0400

    feat: extend Tailwind with zinc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+import colors from 'tailwindcss/colors'
 import tailwindcssAnimate from 'tailwindcss-animate'
 
 export default {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        zinc: colors.zinc,
+      },
+    },
   },
   plugins: [tailwindcssAnimate],
 }
